d1058a0c5f341aa520bd8c2314201afd2b2195c6
Cassen Gerber <cassenpt@gmail.com>
2020-11-20 14:55:28 -0600

    initial file structure complete, connects to db

---
+++ server/modules/pool.js
@@ -0,0 +1,24 @@
+const pg = require('pg');
+
+// get the Pool object from pg
+const Pool = pg.Pool;
+
+// Make our own instance of a Pool from that template Pool object
+const pool = new Pool({
+    database: 'todo_tasks', // Add in database name
+    host: 'localhost', // connect to our local computer
+    port: 5432, // port number, this is the default
+    max: 10, // max number of connections
+    idleTimeoutMillis: 30000 // 30 sec
+})
+
+// When we connect to the database, fun a function
+pool.on('connect', () => {
+    console.log(`Connected to database...`);
+})
+
+pool.on('ERROR', (error) => {
+    console.log('Error from pg', error);
+})
+
+module.exports = pool;
---
+++ server/public/scripts/client.js
@@ -0,0 +1,6 @@
+$(document).ready(onReady);
+
+// Make onReady function to check if jquery is working
+function onReady() {
+    console.log('jquery ready');
+}

f4a78e3282a75eb1c326bf66817324d41f869498
Cassen Gerber <cassenpt@gmail.com>
2020-11-23 11:40:51 -0600

    changed pool.js file for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -4,13 +4,26 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 // Make our own instance of a Pool from that template Pool object
-const pool = new Pool({
-    database: 'weekend-to-do-app', // Add in database name
-    host: 'localhost', // connect to our local computer
-    port: 5432, // port number, this is the default
-    max: 10, // max number of connections
-    idleTimeoutMillis: 30000 // 30 sec
-})
+let pool = {}
+
+if (process.env.DATABASE_URL) {
+    // Running remote (heroku)
+    config = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthenticated: false}
+    }
+} else {
+    confiv = {
+        database: 'weekend-to-do-app', // Add in database name
+        host: 'localhost', // connect to our local computer
+        port: 5432, // port number, this is the default
+        max: 10, // max number of connections
+        idleTimeoutMillis: 30000 // 30 sec
+    };
+}
+
+//Create the pool with the proper config
+const pool = new Pool(config);
 
 // When we connect to the database, fun a function
 pool.on('connect', () => {
